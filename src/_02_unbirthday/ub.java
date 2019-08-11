7572268abcdfa622b98c1123ca2464e8060b7bf5
League Student <league@iMac.attlocal.net>
2019-08-11 13:00:17 -0700

    Create ub.java

---
+++ src/_02_unbirthday/ub.java
@@ -0,0 +1,15 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class ub {
+public static void main(String[] args) {
+	String input = JOptionPane.showInputDialog("when is your birthday?");
+if(input.equals("8/11")){
+	JOptionPane.showMessageDialog(null,"you are terrible and worth a peace of dust");
+}
+else {
+	JOptionPane.showMessageDialog(null,"WELL I DONT CARE IF IT IS YOUR BIRTHDAY. IS IS CRISMESS SO MERRY CRISMESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS SORRY FOR BAD SPLLIN ");
+}
+}
+}
