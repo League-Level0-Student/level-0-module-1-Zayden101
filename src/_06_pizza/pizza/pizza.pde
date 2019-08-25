ccfa3b766a6a3196b394ea6db22075b42495960f
League Student <league@iMac-10.attlocal.net>
2019-08-25 12:58:51 -0700

    ttttttttttt

--- src/_06_pizza/pizza/pizza.pde
+++ src/_06_pizza/pizza/pizza.pde
@@ -1,7 +1,24 @@
 
+int x=0;
+int y=0;
+PImage doge;
 void setup() {
-    
+ doge = loadImage("DOGEEEEEEE.jpg");
+ doge.resize(width,height);
+  size(1900, 1010);
+  background(doge);
+  fill(#521D17);
+  ellipse (800,500,750,750); 
+ fill(#F2FC66);
+ ellipse (800,500,650,650); 
 }
 void draw() {
-    
+
+doge = loadImage("DOGEEEEEEE.jpg");
+doge.resize(50, 50);
+if(mousePressed){
+x= mouseX;
+y=mouseY;
+  }
+  image(doge, x,y);
 }
--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,15 +1,20 @@
 package _07_riddler;
-//    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
+
+import javax.swing.JOptionPane;
+
+//////////  Copyright (c) The League of Amazing Programmers 2013-2019
+//////////  Level 0
 
 
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
+int score=-99999;
+		//  1. Make a variable to hold the score
 
-		// 1. Make a variable to hold the score
 
+	
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
@@ -19,7 +24,10 @@ public class TheRiddler {
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+	JOptionPane.showMessageDialog(null,score);	
+	
+	
+	
 	}
 }
 
