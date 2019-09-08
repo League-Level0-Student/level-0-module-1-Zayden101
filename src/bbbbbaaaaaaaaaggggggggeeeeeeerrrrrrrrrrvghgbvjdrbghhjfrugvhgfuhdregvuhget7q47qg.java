9ecda3d7b7798edb9a2bbbc47f640f8efc704837
League Student <league@iMac-10.attlocal.net>
2019-09-08 12:59:33 -0700

    dvx fdhffmnsn dfsjhf n huyj ç∂mncf mndb bkjsm 3çuew37reo3ibg39p  q'ow 'l45i e ∑slt a;eicfir ;hl˚∆˚¬∆∆˙ƒ¨∂ß   ß ≤≤ç≈ç˙¨ˆ©∆˚©®√©ƒ˙¨†¬¨¶©

---
+++ src/bbbbbaaaaaaaaaggggggggeeeeeeerrrrrrrrrrvghgbvjdrbghhjfrugvhgfuhdregvuhget7q47qg.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class bbbbbaaaaaaaaaggggggggeeeeeeerrrrrrrrrrvghgbvjdrbghhjfrugvhgfuhdregvuhget7q47qg {
+public static void main(String[] args) {
+	int badger = 12;
+	for (int i = 0; i < badger; i++) {
+	JOptionPane.showMessageDialog(null," badger");
+	}
+	int mursshroome= 2;
+	for (int j = 0; j < mursshroome; j++) {
+		JOptionPane.showMessageDialog(null,"mursshroome");
+
+	
+	
+												           
+											
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+}
+}
---
+++ src/myage.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class myage {
+public static void main(String[] args) {
+	
+	
+for (int i = 0; i < 11; i++)
+	JOptionPane.showMessageDialog(null, "My age is " + i);
+	
+	
+	
+	
+	
+	
+}
+}
+
